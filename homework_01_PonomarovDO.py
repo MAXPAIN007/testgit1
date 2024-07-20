7854fc84ec30e308af6d238e6eefc0a02bfadcb1
DmytroPonomarov <krmetalist@gmail.com>
2024-07-20 18:17:49 +0300

    add spase 2

--- homework_01_PonomarovDO.py
+++ homework_01_PonomarovDO.py
@@ -2,11 +2,13 @@
 print("Hello", end = " ")
 print("world!")
 
+
 # task 02 == Виправте синтаксичні помилки
 hello = "Hello"
 world = "world"
 if True:
     print(f"{hello} {world}!")
+
 
 # task 03  == Вcтавте пропущену змінну у ф-цію print
 for letter in "Hello world!":
@@ -17,16 +19,19 @@ for letter in "Hello world!":
 apples = 2
 banana = apples * 4
 
+
 # task 05 == виправте назви змінних
 storona_1 = 1
 storona_2 = 2
 storona_3 = 3
 storona_4 = 4
 
+
 # task 06 == Порахуйте периметр фігури з task 05
 # та виведіть його для користувача
 perimetery = storona_1 + storona_2 + storona_3 + storona_4
 print(perimetery)
+
 
 
 """
